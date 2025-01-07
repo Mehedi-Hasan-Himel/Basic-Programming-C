b000fcf0df01a30492f10b11ebcfcd14d69baf9d
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-01-07 13:24:40 +0600

    double

---
+++ double.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+   double d = 23.123456789;
+
+   printf("PI = %0.3lf", d);
+}
---
+++ solution.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+   int a=3, b=5, c=2;
+   int sol = a + (b - c);
+
+   printf("%d\n",sol);
+}
